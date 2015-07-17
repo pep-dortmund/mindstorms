fecf22a872c26b1e373c3e4c91854c25c630379c
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2015-07-17 22:19:22 +0200

    add keep_distance

---
+++ python-examples/keep_distance.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+This script demonstrates how to use multithreading in
+python to do several tasks (nearly) at once.
+In this example, we let the TankBot keep a
+distance to a target.
+'''
+from __future__ import print_function, division
+from tankbot import TankBot
+
+
+def keep_distance(bot, target_distance):
+    speed = 0
+    bot.run_direct(speed)
+    while True:
+        current_distance = bot.ultrasonic_sensor.dist_cm
+        difference = current_distance - target_distance
+        if difference > 0:
+            speed = min(5*difference, 100)
+        else:
+            speed = max(200 * difference / target_distance, -100)
+        bot.speed = speed
+
+
+if __name__ == '__main__':
+    try:
+        with TankBot() as bot:
+            keep_distance(bot, 10)
+    except (KeyboardInterrupt, SystemExit):
+        pass
--- python-examples/tankbot.py
+++ python-examples/tankbot.py
@@ -8,16 +8,29 @@ from ev3.ev3dev import Motor
 from ev3.lego import ColorSensor, GyroSensor, UltrasonicSensor
 
 
-class TankBot():
+class TankBot(object):
     def __init__(self):
         # setup the motors
         self.motor_left = Motor(port='B')
         self.motor_right = Motor(port='D')
+        self.motors = (self.motor_left, self.motor_right)
+
+        self._speed = 0
 
         # setup the sensors
         self.color_sensor = ColorSensor(port=1)
         self.ultrasonic_sensor = UltrasonicSensor(port=3)
         self.gyro_sensor = GyroSensor(port=4)
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):
+        for motor in self.motors:
+            motor.duty_cycle_sp = int(-value)
+        self._speed = -value
 
     def turn_seconds(self, seconds, speed):
         self.motor_left.run_time_limited(seconds * 1000, speed)
@@ -29,6 +42,19 @@ class TankBot():
         # means to go forward
         self.motor_left.run_time_limited(seconds * 1000, -speed)
         self.motor_right.run_time_limited(seconds * 1000, -speed)
+
+    def run_forever(self, speed=50):
+        # the TankBot has the motors in backwards direction
+        # so we multiply the speed by -1 so that a positivs speed
+        # means to go forward
+        self.motor_left.run_forever(-speed)
+        self.motor_right.run_forever(-speed)
+
+    def run_direct(self, speed=50):
+        for motor in self.motors:
+            motor.mode = 'run-direct'
+            motor.duty_cycle_sp = -int(speed)
+            motor.write_value('command', 'run-direct')
 
     def stop(self):
         self.motor_left.stop()
